33381441fda96a80aa85775395682b68b6af0e1a
Subhadip Dutta <subhadip.dutta@quickelf.com>
2026-05-31 16:03:37 +0530

    refactor: update Prisma configuration to use DATABASE_URL for database connection

--- src/core/prisma/prisma.service.ts
+++ src/core/prisma/prisma.service.ts
@@ -7,11 +7,7 @@ import { Pool } from 'pg';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const pool = new Pool({
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      connectionString: process.env['DATABASE_URL'],
       ssl: {
         rejectUnauthorized: false,
       },
