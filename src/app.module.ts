cc602ccfa92cd53f0afa2d788967bc245bcef438
Subhadip Dutta <subhadip.dutta@quickelf.com>
2026-08-10 22:17:33 +0530

    fix: update ConfigModule to specify envFilePath and adjust .gitignore for environment files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
-import { ProfileModule } from './modules/profile/profile.module';
 import { FirebaseModule } from './core/firebase/firebase/firebase.module';
+import { ProfileModule } from './modules/profile/profile.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env.dev' }),
     DatabaseModule,
     ProfileModule,
     FirebaseModule,
